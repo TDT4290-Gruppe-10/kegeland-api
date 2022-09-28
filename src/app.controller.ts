1bc1c6c8343eeb807ad99037e01530d4753e3f74
Simen Kristoffersen <70207504+simenkristoff@users.noreply.github.com>
2022-09-29 00:35:16 +0100

    Added OpenApi-documentation (#5)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
+import { FirebaseAuthGuard } from './firebase/firebase-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiBearerAuth('access-token')
+  @UseGuards(FirebaseAuthGuard)
   getHello() {
     return this.appService.getHello();
   }
