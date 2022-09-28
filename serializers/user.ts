f14ac2942c9eb9c43e6a314c3a5af6bd5727b050
Simen Kristoffersen <70207504+simenkristoff@users.noreply.github.com>
2022-09-28 23:41:10 +0100

    Tdt 62 setup ci cd pipeline (#4)
    * HOTFIX: Change line-ending in prettier
    
    * Refactored firebase-admin to global service
    
    * Added passport and class-transformer
    
    * Added custom configuration for firebase
    
    * Added firestore-sdk to global firestore-provider
    
    * Configured passport-policy for firestore-auth
    
    * Added custom endpoint-guard for firebase-auth
    
    * Added auth-module for auth endpoints
    Currently supporting login and register
    
    * Remove unused imports from main.ts
    
    * Added auto-validation of parameters
    
    * Added return object on successfull authorization
    
    * Cleanup unused-files
    
    * TDT-66 Configured hook for linting pre-commit
    
    * TDT-67 Added CI/CD-workflow
    
    * Added Badges to README.md
    
    * Update yarn.lock

--- serializers/user.ts
+++
@@ -1,9 +0,0 @@
-import { User, UserSettings } from "../models/user"
-
-export const userDeSerializer = (body: JSON) => {
-    try {
-        console.log(body)
-    } catch (e) {
-        console.log(e)
-    }
-}
