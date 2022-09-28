3fc89e480fcedff4486f0090ef48ccc296445c76
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-28 18:12:30 +0100

    TDT-64 Connected the api to firestore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeApp } from 'firebase-admin/app';
+import { credential } from 'firebase-admin';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+
+  // Initialize Firestore
+  initializeApp({
+    credential: credential.cert(
+      configService.get<string>('GOOGLE_APPLICATION_CREDENTIALS'),
+    ),
+  });
+
+  await app.listen(configService.get<number>('SERVER_PORT') || 3000);
 }
 bootstrap();
