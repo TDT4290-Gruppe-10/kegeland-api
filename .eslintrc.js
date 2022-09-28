287c2eea7785e29144dd2b8274ca020a547dbd1d
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-28 18:23:54 +0100

    TDT-65 Added import order in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,16 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -21,5 +24,23 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'prettier/prettier': 'error',
+    'import/default': 0,
+    'import/no-unresolved': 0,
   },
 };
