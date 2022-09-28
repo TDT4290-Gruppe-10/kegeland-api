2fb9265358aa88a0a3fd77c7ba2aba8537727e70
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-28 21:47:03 +0100

    Added auto-validation of parameters

--- src/auth/dto/register-credentials.dto.ts
+++ src/auth/dto/register-credentials.dto.ts
@@ -1,3 +1,8 @@
+import { MinLength } from 'class-validator';
+
 import { LoginCredentials } from './login-credentials.dto';
 
-export class RegisterCredentials extends LoginCredentials {}
+export class RegisterCredentials extends LoginCredentials {
+  @MinLength(6)
+  password: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService: ConfigService = app.get(ConfigService);
 
   await app.listen(configService.get<number>('SERVER_PORT') || 3000);
