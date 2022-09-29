9c3f383d4157c54c733bef2b2a884fec3f23f37b
Simen Kristoffersen <70207504+simenkristoff@users.noreply.github.com>
2022-09-29 12:03:55 +0100

    Tdt 68 add support for refresh tokens (#6)
    * Set config in firebase.service to readonly
    
    * Reformat UserEntity
    
    * Added entity for token credentials
    
    * Installed required axios-packages
    
    * Added functionality for refreshing tokens
    
    * Added endpoint for refreshing of tokens
    
    * Update .env.example
    
    * Update .env.example
    
    * Resolve merge conflict
    
    * Update README.md

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -2,9 +2,6 @@ import { Exclude } from 'class-transformer';
 import { ApiHideProperty } from '@nestjs/swagger';
 
 export class UserEntity {
-  @Exclude()
-  @ApiHideProperty()
-  kind: string;
   /**
    * @example 'ola.nordmann@gmail.com'
    */
@@ -14,15 +11,20 @@ export class UserEntity {
    */
   displayName: string | null;
   idToken: string;
-  @Exclude()
-  @ApiHideProperty()
-  registered: boolean;
   refreshToken: string;
   /**
    * The tokens TTL in seconds
    * @example 3600
    */
   expiresIn: number;
+
+  @Exclude()
+  @ApiHideProperty()
+  kind: string;
+
+  @Exclude()
+  @ApiHideProperty()
+  registered: boolean;
 
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
