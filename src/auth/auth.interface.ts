3d4d91d29e5a8d10b04807ff6bc4122e1717f080
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-29 11:19:48 +0100

    Added functionality for refreshing tokens

--- src/auth/auth.interface.ts
+++ src/auth/auth.interface.ts
@@ -5,3 +5,13 @@ import { UserEntity } from './entities/user.entity';
 export interface AuthResponse extends UserCredential {
   _tokenResponse: Partial<UserEntity>;
 }
+
+export type RefreshErrorResponse = {
+  error: {
+    message:
+      | 'TOKEN_EXPIRED'
+      | 'USER_DISABLED'
+      | 'USER_NOT_FOUND'
+      | 'INVALID_REFRESH_TOKEN';
+  };
+};
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,20 +4,34 @@ import {
   InternalServerErrorException,
   ServiceUnavailableException,
   UnauthorizedException,
+  Inject,
 } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { FirebaseError } from 'firebase/app';
+import firebaseConfig from 'src/config/firebase.config';
+import { ConfigType } from '@nestjs/config';
+
+import { isAxiosError } from '../utils/isAxiosError';
 
 import { LoginCredentials } from './dto/login-credentials.dto';
 import { RegisterCredentials } from './dto/register-credentials.dto';
-import { AuthResponse } from './auth.interface';
+import { AuthResponse, RefreshErrorResponse } from './auth.interface';
+import { RefreshToken } from './dto/refresh-token.dto';
+import { TokenCredentials } from './entities/token-credentials.entity';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @Inject(firebaseConfig.KEY)
+    private readonly config: ConfigType<typeof firebaseConfig>,
+    private readonly httpService: HttpService,
+  ) {}
+
   async login(loginCredentials: LoginCredentials) {
     const { email, password } = loginCredentials;
     const auth = getAuth();
@@ -74,6 +88,36 @@ export class AuthService {
         throw new InternalServerErrorException(
           'An unknown error has occurred.',
         );
+      });
+  }
+
+  async refresh({ refreshToken }: RefreshToken) {
+    return this.httpService.axiosRef
+      .post<TokenCredentials>(
+        `https://securetoken.googleapis.com/v1/token?key=${this.config.sdk.apiKey}`,
+        {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+        },
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        if (isAxiosError<RefreshErrorResponse>(err)) {
+          const { message } = err.response.data.error;
+          switch (message) {
+            case 'USER_DISABLED':
+              throw new BadRequestException('This user has been disabled.');
+            case 'INVALID_REFRESH_TOKEN':
+              throw new BadRequestException('Invalid refresh token.');
+            case 'TOKEN_EXPIRED':
+              throw new BadRequestException('The refresh token has expired.');
+            case 'USER_NOT_FOUND':
+              throw new BadRequestException(
+                'The requested user credentials does not exist.',
+              );
+          }
+        }
+        throw new InternalServerErrorException();
       });
   }
 }
