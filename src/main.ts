192e6e8a107307888b06ee6934cb55fd1e81cd60
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-30 10:42:11 +0100

    Added middleware for stripping unwanted request-params

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Kegeland API')
