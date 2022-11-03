d8aa99e43c53b11564524103ff3572abb2a986cf
FabianFoss <fabianfossbudal@hotmail.com>
2022-11-03 11:18:58 +0100

    feat: adds role protection for API endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Delete,
+  UseGuards
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-
 import { FirebaseAuthGuard } from '../firebase/firebase-auth.guard';
 import { Role } from '../roles/enums/role.enum';
 import { Roles } from '../roles/roles.decorator';
 import { RolesGuard } from '../roles/roles.guard';
-
 import { UsersService } from './users.service';
 
 @Controller('users')
