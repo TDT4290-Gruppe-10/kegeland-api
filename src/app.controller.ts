00013d47c3688f6f442b4781113ea23979c0f5c5
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-15 13:37:02 +0100

    Lint errors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Redirect } from '@nestjs/common/decorators';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
